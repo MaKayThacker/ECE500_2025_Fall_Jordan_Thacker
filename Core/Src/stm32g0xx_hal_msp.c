6e6c1838ef0178f02c9800f5094ac85344dfe2ca
JaThac <Jathac02@louisville.edu>
2025-11-17 18:18:44 -0500

    adding Mod 8, Still not done due to the Encoding on the motor and the RPM caluations being not complete

--- Core/Src/stm32g0xx_hal_msp.c
+++ Core/Src/stm32g0xx_hal_msp.c
@@ -156,6 +156,36 @@ void HAL_SPI_MspDeInit(SPI_HandleTypeDef* hspi)
 }
 
 /**
+  * @brief TIM_Base MSP Initialization
+  * This function configures the hardware resources used in this example
+  * @param htim_base: TIM_Base handle pointer
+  * @retval None
+  */
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
+{
+  if (htim_base->Instance == TIM14)
+  {
+    /* Enable TIM14 clock for base/PWM operation */
+    __HAL_RCC_TIM14_CLK_ENABLE();
+  }
+}
+
+/**
+  * @brief TIM_Base MSP De-Initialization
+  * This function freeze the hardware resources used in this example
+  * @param htim_base: TIM_Base handle pointer
+  * @retval None
+  */
+void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef* htim_base)
+{
+  if (htim_base->Instance == TIM14)
+  {
+    /* Disable TIM14 clock */
+    __HAL_RCC_TIM14_CLK_DISABLE();
+  }
+}
+
+/**
   * @brief UART MSP Initialization
   * This function configures the hardware resources used in this example
   * @param huart: UART handle pointer
@@ -233,5 +263,43 @@ void HAL_UART_MspDeInit(UART_HandleTypeDef* huart)
 }
 
 /* USER CODE BEGIN 1 */
+
+/**
+  * @brief TIM PWM MSP Initialization
+  * @param htim_pwm: TIM handle pointer
+  * @retval None
+  */
+void HAL_TIM_PWM_MspInit(TIM_HandleTypeDef* htim_pwm)
+{
+  if (htim_pwm->Instance == TIM14)
+  {
+    __HAL_RCC_TIM14_CLK_ENABLE();
+  }
+}
+
+/**
+  * @brief TIM MSP Post Initialization
+  *        Configure GPIO for TIM14_CH1 on PA4 (MOTOR_PWM)
+  * @param htim: TIM handle pointer
+  * @retval None
+  */
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef* htim)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  if (htim->Instance == TIM14)
+  {
+    /* Make sure GPIOA clock is enabled for PA4 */
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+
+    /* MOTOR_PWM: PA4 -> TIM14_CH1, AF4 */
+    GPIO_InitStruct.Pin = MOTOR_PWM_Pin;          // PA4
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Alternate = GPIO_AF4_TIM14;   // TIM14 CH1 alternate function
+    HAL_GPIO_Init(MOTOR_PWM_GPIO_Port, &GPIO_InitStruct);
+  }
+}
 
 /* USER CODE END 1 */
